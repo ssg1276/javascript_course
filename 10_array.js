40999431b8b11faed625582622236211942d7e5e
Sahil Singh Guleria <135002064+ssg1276@users.noreply.github.com>
2024-01-06 09:21:44 +0000

    array-1

---
+++ 10_array.js
@@ -0,0 +1,18 @@
+//array 
+
+const array=[0,1,2,3,4,5];
+const new1= new Array(1,23,4,5); // adds square brackets by itself 
+
+//accessing value
+console.log(array[0]); 
+
+
+//array methods 
+new1.push(10) //adding the at the end of the array 
+new1.pop()// removing the element form the end 
+new1.unshift(9) // adds the element the starting of the array
+new1.shift()// remove the element from the starting of the array
+const new2=new1.join() // it changes the data type of the new2 into string 
+
+console.log(new1);
+
