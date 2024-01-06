caeefa3b5605c83e386e2871ac50cb969d5d3eb8
Sahil Singh Guleria <135002064+ssg1276@users.noreply.github.com>
2024-01-06 08:48:33 +0000

    datetime

---
+++ 09_dateandtime.js
@@ -0,0 +1,15 @@
+let myDate=new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+let newDate=new Date()
+// let abc=newDate.toLocaleString('default',{
+//        weekday:"long",
+//        month:"long",
+//        year:"numeric"
+// })
+// console.log(newDate.getMinutes());
+
+// in javascript month start with 0 if using single digit and if using double digit it start with 01
+
