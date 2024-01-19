06b4a798bb17ca0f6e068a61ad01dcb76599b675
Sahil Singh Guleria <135002064+ssg1276@users.noreply.github.com>
2024-01-19 01:56:41 +0000

    foreach

---
+++ 29_foreachloop.js
@@ -0,0 +1,54 @@
+//for each loop
+
+//syntax
+// array.forEach(element => {
+    
+// });
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+//hihgh order function --->"." dot operator
+//callback function is used here 
+
+ coding.forEach( function (val){
+     console.log(val);
+ } )
+
+
+
+ //using arrow function syntax
+ coding.forEach( (item) => {
+     console.log(item);
+ } )
+
+// callback function declaring outside 
+ function printMe(item){
+     console.log(item);
+ }
+
+ coding.forEach(printMe) //giving only reference
+
+ 
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
